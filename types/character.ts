13e7d247b222b8e8ac651428514b50182f72aa1e
Yejin <mingmi_27@naver.com>
2026-01-02 19:10:35 +0900

    feat : 배경타입, 동물타입추가

--- types/character.ts
+++ types/character.ts
@@ -19,25 +19,76 @@ export const ANIMALS = [
   {
     type: 'capybara',
     id: 'char-1',
-    src: '/character/Capybara.svg',
+    src: '/character/Capybara.png',
     name: '카피바라',
   },
   {
     type: 'cat',
     id: 'char-2',
-    src: '/character/Cat.svg',
+    src: '/character/Cat.png',
     name: '고양이',
   },
   {
-    type: 'whale',
+    type: 'chick',
     id: 'char-3',
-    src: '/character/Whale.svg',
+    src: '/character/Chick.png',
+    name: '병아리',
+  },
+  {
+    type: 'dog',
+    id: 'char-4',
+    src: '/character/Dog.png',
+    name: '강아지',
+  },
+  {
+    type: 'platypus',
+    id: 'char-5',
+    src: '/character/Platypus.png',
+    name: '오리너구리',
+  },
+  {
+    type: 'whale',
+    id: 'char-6',
+    src: '/character/Whale.png',
     name: '고래',
   },
   {
     type: 'wolf',
-    id: 'char-4',
-    src: '/character/Wolf.svg',
+    id: 'char-7',
+    src: '/character/Wolf.png',
     name: '늑대',
+  },
+  {
+    type: 'weasel',
+    id: 'char-8',
+    src: '/character/Weasel.png',
+    name: '족제비',
+  },
+];
+
+export const BACKGROUND = [
+  {
+    type: 'club',
+    id: 'bg-1',
+    src: '/bg/Club.jpg',
+    name: '클럽',
+  },
+  {
+    type: 'park',
+    id: 'bg-2',
+    src: '/bg/Park.jpg',
+    name: '공원',
+  },
+  {
+    type: 'redcarpet',
+    id: 'bg-3',
+    src: '/bg/Redcarpet.jpg',
+    name: '레드카펫',
+  },
+  {
+    type: 'room',
+    id: 'bg-4',
+    src: '/bg/Room.jpg',
+    name: '방',
   },
 ];
