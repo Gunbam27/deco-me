51aef8e64fdbaca5100a14718e3e5e7bafe78919
Yejin <mingmi_27@naver.com>
2026-01-13 00:26:50 +0900

    chore : add font-family, add lucid-react, add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-jua)', 'system-ui', 'sans-serif'],
+      },
       colors: {
         brown: {
           50: '#fff5f0',
@@ -12,7 +15,9 @@ module.exports = {
           700: '#4a1f0d',
         },
         pink: {
-          100: '#fde2e7',
+          100: '#fff2f2',
+          200: '#fde2e7',
+          500: '#fcbfc3',
         },
       },
     },
