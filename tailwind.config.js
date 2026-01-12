6eee3c914b5057c94b76e6194668d38f25cde25a
Yejin <mingmi_27@naver.com>
2026-01-12 23:12:59 +0900

    chore : add themeColor, add logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brown: {
+          50: '#fff5f0',
+          100: '#f9d7c6',
+          300: '#c97a4a',
+          500: '#7c3b18',
+          700: '#4a1f0d',
+        },
+        pink: {
+          100: '#fde2e7',
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
