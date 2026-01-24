0cf25a55e9b752d1ba0a28229c8caf9c0e652ec7
Yejin <mingmi_27@naver.com>
2026-01-25 01:29:50 +0900

    fix : 상수변경

--- util/canvasConfig.ts
+++ util/canvasConfig.ts
@@ -4,5 +4,5 @@ export const CANVAS_CHARACTER_RATIO = 1.2;
 export const SPEECH_BUBBLE_X_RATIO = 0.70;
 export const SPEECH_BUBBLE_Y_RATIO = 0.1;
 export const SPEECH_BUBBLE_WIDTH = 82;
-export const SPEECH_BUBBLE_HEIGHT = 50;
+export const SPEECH_BUBBLE_HEIGHT = 60;
 export const SPEECH_BUBBLE_FONT_SIZE = 17;
